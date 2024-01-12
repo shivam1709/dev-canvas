592599b2b39c72528f8871afc3663f8d1575d461
shivam1709 <68727150+shivam1709@users.noreply.github.com>
2024-01-12 17:55:33 -0500

    Added Fav icon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+//import myImage from './assets/DevCanvas.png'; // Adjust the path based on your folder structure
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div></div>
+    //   <img class="logo"src={myImage} alt='app logo' />
+    
   );
 }
 
